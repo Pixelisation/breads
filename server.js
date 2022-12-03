ffdb78aa5ea8bf2b77753a5e833e5e974179c245
torian austin <torian03@outlook.com>
2022-12-03 10:12:24 -0800

    mongoose

--- server.js
+++ server.js
@@ -1,35 +1,43 @@
 // DEPENDENCIES
-const express = require('express')
-const methodOverride = require('method-override')
+const express = require("express");
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
 
 // CONFIGURATION
-require('dotenv').config()
-const PORT = process.env.PORT
-const app = express()
+require("dotenv").config();
+const PORT = process.env.PORT;
+const app = express();
 
 // MIDDLEWARE
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
-app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
-app.use(methodOverride('_method'))
+app.set("views", __dirname + "/views");
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("connected to mongo: ", process.env.MONGO_URI);
+  }
+);
 
 // ROUTES
-app.get('/', (req, res) => {
-  res.send('Welcome to an Awesome App about Breads')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to an Awesome App about Breads");
+});
 
 // Breads ROUTE
-const breadsController = require('./controllers/breads_controller.js')
-app.use('/breads', breadsController)
+const breadsController = require("./controllers/breads_controller.js");
+app.use("/breads", breadsController);
 
 // 404 Page
-app.get('*', (req, res) => {
-  res.send('404')
-})
+app.get("*", (req, res) => {
+  res.send("404");
+});
 
 // LISTEN
 app.listen(PORT, () => {
-  console.log('listening on port', PORT);
-})
+  console.log("listening on port", PORT);
+});
