e8751bf5bd9378052f3dca828c65965e80420d78
torian austin <torian03@outlook.com>
2022-12-10 10:42:56 -0800

    fix some virtuals

--- controllers/bakers_controller.js
+++ controllers/bakers_controller.js
@@ -4,11 +4,6 @@ const baker = express.Router()
 const Baker = require('../models/baker.js')
 const bakerSeedData = require('../models/baker_seed.js')
 
-baker.get('/data/seed', (req, res) => {
-  Baker.insertMany(bakerSeedData)
-      .then(res.redirect('/breads'))
-})
-
 // Index
 baker.get('/', (req, res) => {
   Baker.find()
@@ -29,6 +24,10 @@ baker.get('/:id', (req, res) => {
       })
 })
 
+baker.get('/data/seed', (req, res) => {
+  Baker.insertMany(bakerSeedData)
+      .then(res.redirect('/breads'))
+})
 
 // export
 module.exports = baker                    
